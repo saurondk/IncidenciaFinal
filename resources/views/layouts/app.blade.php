254daa6f00592fc24fc213eea0299ff9e9fb41d2
2DAW12 <2DAW12@DOMDPTOINFJMPP.INF>
2023-02-27 15:54:08 +0100

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,8 +76,8 @@
                 </li>
               </ul>
               
-              <li class="dropdown mx-2" >
-                  <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+              <li class="dropdown ms-2" >
+                  <a id="navbarDropdown" class="nav-link dropdown-toggle ms-2" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                       <button class="btn btn-danger">{{ Auth::user()->name }}</button>  
                   </a>
                   <br>
